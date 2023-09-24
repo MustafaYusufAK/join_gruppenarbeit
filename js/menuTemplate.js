948c142db4d8f7c692833b09e33e0b81b9ee3ae0
AntonOsipov99 <antonosipov@gmx.de>
2023-09-24 22:39:04 +0200

    update

--- js/menuTemplate.js
+++ js/menuTemplate.js
@@ -3,7 +3,7 @@ function generateSideBar() {
     let menu = document.getElementById('content');
     menu.innerHTML = /*html*/ `<nav><img src="../assets/img/Capa 2.svg" class="logo"><div class="nav-flex-box"><div class="flex-box-mobile"><a href="../html/summary.html" class="nav-sub">
     <img src="../assets/img/Icons.svg">Summary</a><a href="../html/add_task.html" class="nav-sub"><img src="../assets/img/Icons (1).svg">Add Task</a><a href="../html/board.html" class="nav-sub">
-    <img src="../assets/img/Icons (2).svg">Board</a><a href="../html/contacts.html" class="contact-background contacts-background"><img src="../assets/img/Icons (3).svg">Contacs</a></div><div class="nav-bottom">
+    <img src="../assets/img/Icons (2).svg">Board</a><a href="../html/contacts.html" class="contact-background contacts-background"><img src="../assets/img/Icons (3).svg">Contacts</a></div><div class="nav-bottom">
     <a href="#" class="nav-bottom-a">Privacy Policy</a><a href="#" class="nav-bottom-a">Legal Notice</a></div></div></nav>`;
     generateHeader(menu);
     showContacts();
