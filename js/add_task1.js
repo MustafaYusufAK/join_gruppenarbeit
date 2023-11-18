6df30b39fa709ab6ca0eba2aff3a21e494e45625
JanTHorstmann <janhorstmann@yahoo.de>
2023-11-18 16:54:25 +0100

    cleanCode add_task1.js

--- js/add_task1.js
+++ js/add_task1.js
@@ -1,7 +1,6 @@
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
-
     do {
         color = '#';
         for (let i = 0; i < 6; i++) {
@@ -9,48 +8,41 @@ function getRandomColor() {
             color += randomLetter;
         }
     } while (isTooLight(color));
-
     return color;
 }
 
 function isTooLight(color) {
-    // Berechne die Helligkeit der Farbe (YIQ-Berechnung)
-    const hex = color.substring(1); // Entferne das führende #
+    const hex = color.substring(1); // remove # from Colorcode
     const r = parseInt(hex.slice(0, 2), 16);
     const g = parseInt(hex.slice(2, 4), 16);
     const b = parseInt(hex.slice(4, 6), 16);
     const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
-
-    // Eine Farbe ist zu hell, wenn ihre Helligkeit über einem bestimmten Schwellenwert liegt
     return yiq > 128;
 }
 
 function createContactDropdown() {
     let dropdown = document.getElementById('which_assigned_contact');
-
-    // Entferne vorhandene Optionen
     dropdown.innerHTML = '';
-
-    // Füge eine Standardoption hinzu
     let defaultOption = document.createElement('option');
     defaultOption.value = 'Select contacts to assign';
     defaultOption.textContent = 'Select contacts to assign';
     dropdown.appendChild(defaultOption);
-
-    // Hinzufügen eines Event-Listeners, um die Option auszublenden
     dropdown.addEventListener('change', (event) => {
-        const selectedOption = event.target.options[event.target.selectedIndex];
+        removeOptionDropdownContacts(event);
 
-        // Überprüfe, ob die ausgewählte Option nicht die Standardoption ist
-        if (selectedOption.value !== 'Select contacts to assign') {
-            selectedOption.classList.add('d-none');  // Füge die Klasse hinzu
-
-            // Rufe die Funktion createAssigneeBall auf und übergebe den Value
-            createAssigneeBall(selectedOption.textContent, selectedOption.value);
-        }
     });
+    createOptionDropdownContacts(dropdown);
+}
 
-    // Füge Optionen für jeden Kontakt hinzu
+function removeOptionDropdownContacts(event) {
+    const selectedOption = event.target.options[event.target.selectedIndex];
+    if (selectedOption.value !== 'Select contacts to assign') {
+        selectedOption.classList.add('d-none');
+        createAssigneeBall(selectedOption.textContent, selectedOption.value);
+    }
+}
+
+function createOptionDropdownContacts(dropdown) {
     contacts.forEach(contact => {
         let option = document.createElement('option');
         option.value = contact.name;
@@ -63,62 +55,55 @@ function createAssigneeBall(contactName, contactValue) {
     let assignedToList = document.getElementById('assignedToList');
     let assigneeContainer = document.createElement('div');
     assigneeContainer.classList.add('assigneeContainer');
-
-    // Teile den Namen in Vorname und Nachname
-    let [firstName, lastName] = contactName.split(' ');
-
-    // Füge die Anfangsbuchstaben hinzu
-    assigneeContainer.innerHTML = `${firstName.charAt(0)}${lastName ? lastName.charAt(0) : ''}`;
-
-    // Füge den Value als Attribut hinzu
-    assigneeContainer.setAttribute('data-contact-value', contactValue);
-
-    // Rufe die getRandomColor-Funktion auf, um die Hintergrundfarbe zu erhalten
+    getInitalsFromContact(contactName, contactValue, assigneeContainer);
     let backgroundColor = getRandomColor();
-    assigneeContainer.style.backgroundColor = backgroundColor; // Setze die Hintergrundfarbe
-
-    // Event-Listener, um den Ball zu entfernen und die zugehörige Option freizugeben
+    assigneeContainer.style.backgroundColor = backgroundColor;
     assigneeContainer.addEventListener('click', () => {
         assigneeContainer.remove();
-
-        // Finde die Option im Dropdown anhand des Kontakt-Werts
-        let option = Array.from(document.getElementById('which_assigned_contact').options)
-            .find(opt => opt.value === contactValue);
-
-        // Wenn die Option gefunden wurde, entferne die Klasse 'd-none'
-        if (option) {
-            option.classList.remove('d-none');
-        }
+        removeAssigneeContainer(contactValue);
     });
+    eventListenerMouseOver(assigneeContainer);
+    eventListenerMouseOut(assigneeContainer);
+    assignedToList.appendChild(assigneeContainer);
+}
 
-    // Event-Listener für hover
+function getInitalsFromContact(contactName, contactValue, assigneeContainer) {
+    let [firstName, lastName] = contactName.split(' ');
+    assigneeContainer.innerHTML = `${firstName.charAt(0)}${lastName ? lastName.charAt(0) : ''}`;
+    assigneeContainer.setAttribute('data-contact-value', contactValue);
+}
+
+function removeAssigneeContainer(contactValue) {
+    let option = Array.from(document.getElementById('which_assigned_contact').options)
+        .find(opt => opt.value === contactValue);
+    if (option) {
+        option.classList.remove('d-none');
+    }
+}
+
+function eventListenerMouseOver(assigneeContainer) {
     assigneeContainer.addEventListener('mouseover', (event) => {
-        // Zeige den Kontaktwert als Tooltip an
         const contactValue = event.target.getAttribute('data-contact-value');
         event.target.setAttribute('title', `${contactValue}`);
     });
+}
 
-    // Event-Listener um Tooltip auszublenden
+function eventListenerMouseOut(assigneeContainer) {
     assigneeContainer.addEventListener('mouseout', (event) => {
-        // Entferne den Tooltip, wenn der Hover beendet ist
         event.target.removeAttribute('title');
     });
-
-    assignedToList.appendChild(assigneeContainer);
 }
 
 function checkCreatedAt() {
     const mustHaveArrays = [
         createdAtArray,
     ];
-
     for (const array of mustHaveArrays) {
         if (array.length === 0) {
             showNotification();
             return false;
         }
     }
-
     return true;
 }
 
@@ -127,14 +112,12 @@ function checkMustHaveEntriesText() {
         titlesArray,
         descriptionsArray,
     ];
-
     for (const array of mustHaveArrays) {
         if (!containsLetter(array)) {
             showSecondNotification();
             return false;
         }
     }
-
     return true;
 }
 
@@ -152,26 +135,30 @@ function redirectToBoard() {
     let userName = getUserName();
     const link = `./board.html?msg=Welcomme to Join, ${userName}`;
     window.location.href = link;
-
 }
 
-function checkMustHaveEntries() {
-    console.log('Checking must have entries...');
-    const mustHaveArrays = [
-        priorityArray,
-        categoryArray,
-        categoryColorArray,
-        assignedToValuesArray,
-        assignedToColorsArray,
-        assignedShortValues,
-    ];
 
-    for (const array of mustHaveArrays) {
-        if (array.length === 0) {
-            showNotification();
-            return false;
-        }
-    }
 
-    return true;
-}
+// noch nötig?
+// function checkMustHaveEntries() {
+//     console.log('Checking must have entries...');
+//     const mustHaveArrays = fillMustHaveArray()
+//     for (const array of mustHaveArrays) {
+//         if (array.length === 0) {
+//             showNotification();
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// function fillMustHaveArray() {
+//      return [
+//         priorityArray,
+//         categoryArray,
+//         categoryColorArray,
+//         assignedToValuesArray,
+//         assignedToColorsArray,
+//         assignedShortValues,
+//     ];
+// }
